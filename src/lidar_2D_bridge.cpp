2a51a548b8712537a033793234620f421d9865d4
Olga Napolitano <olga.napolitano3@gmail.com>
2022-07-27 12:44:37 +0200

    modified lidar2D bridge

---
+++ src/lidar_2D_bridge.cpp
@@ -0,0 +1,47 @@
+
+#include <ros/ros.h>
+
+#include <rpwc_msgs/LaserScanReq.h>
+#include <sensor_msgs/LaserScan.h>
+
+
+sensor_msgs::LaserScan scan_;
+
+bool callbackServerLidar2D(rpwc_msgs::LaserScanReq::Request  &req, rpwc_msgs::LaserScanReq::Response &res)
+{
+    res.scan = scan_;
+	return true;
+}
+
+void callback_scan(const sensor_msgs::LaserScanPtr& msg)
+{
+  scan_ = *msg;
+}
+
+
+int main(int argc, char** argv)
+{
+	//----------------------------------------------------------
+	// Preparations
+	//----------------------------------------------------------
+
+	// Initialize the node.
+	ros::init(argc, argv, "lidar_2D_bridge_node");
+	ros::NodeHandle nh;
+	ros::Rate rate(5);
+
+	ros::ServiceServer serverLidar2D = nh.advertiseService("lidar2D", callbackServerLidar2D);
+
+    ros::Subscriber sub_scan = nh.subscribe("robot/merged_laser/scan", 1, &callback_scan);
+	ros::Publisher LaserScan = nh.advertise<sensor_msgs::LaserScan>("lidar2D", 1);
+	
+	while (ros::ok())
+	{
+		sensor_msgs::LaserScan tmp_data;
+		tmp_data = scan_;
+		LaserScan.publish(tmp_data);
+		ros::spinOnce();
+		rate.sleep();
+	}
+	return 0;
+}
