cfbc6225b4c867aec5008866077aab1ac1708ff6
gialen <g.lentini88@gmail.com>
2022-09-05 17:28:09 +0200

    update softhand research

--- src/main_softhand_bridge.cpp
+++ src/main_softhand_bridge.cpp
@@ -1,24 +1,25 @@
 
 #include <ros/ros.h>
 #include <ros/rate.h>
-#include <rpwc/rpwc_gripper_cmd.h>
+//#include <rpwc/rpwc_gripper_cmd.h>
+#include <rpwc_msgs/RobotEeStateStamped.h>
 #include <std_msgs/Float64.h>
 #include <trajectory_msgs/JointTrajectory.h>
 
 
 trajectory_msgs::JointTrajectory send_hand_;
-ros::Publisher pub_hand_des_, pub_CommandHand_test_;
+ros::Publisher pub_for_recording_, pub_hand_des_, pub_CommandHand_test_;
 
-bool callback_rpwc_gripper_single_cmd(rpwc::rpwc_gripper_cmd::Request  &req, rpwc::rpwc_gripper_cmd::Response &res)
-{
-	send_hand_.points.clear();
-  	send_hand_.points.resize(1);
-  	send_hand_.points[0].positions.push_back(req.EE_cmd_.data);
-  	send_hand_.points[0].time_from_start = ros::Duration(0.3);
-  	pub_hand_des_.publish(send_hand_);
+// bool callback_rpwc_gripper_single_cmd(rpwc::rpwc_gripper_cmd::Request  &req, rpwc::rpwc_gripper_cmd::Response &res)
+// {
+// 	send_hand_.points.clear();
+//   	send_hand_.points.resize(1);
+//   	send_hand_.points[0].positions.push_back(req.EE_cmd_.data);
+//   	send_hand_.points[0].time_from_start = ros::Duration(0.3);
+//   	pub_hand_des_.publish(send_hand_);
 
-	return true;
-}
+// 	return true;
+// }
 
 void callback_rpwc_gripper_cmd(const std_msgs::Float64::ConstPtr& msg)
 {
@@ -29,9 +30,14 @@ void callback_rpwc_gripper_cmd(const std_msgs::Float64::ConstPtr& msg)
   	send_hand_.points[0].time_from_start = ros::Duration(0.3);
   	pub_hand_des_.publish(send_hand_);
 
-  	std_msgs::Float64 hand_test;
-  	hand_test.data = msg->data;
-  	pub_CommandHand_test_.publish(hand_test);
+	rpwc_msgs::RobotEeStateStamped tmpMsg;
+	tmpMsg.data.data = msg->data;
+	tmpMsg.header.stamp = ros::Time::now();
+	pub_for_recording_.publish(tmpMsg);
+
+  	// std_msgs::Float64 hand_test;
+  	// hand_test.data = msg->data;
+  	//pub_CommandHand_test_.publish(hand_test);
 }
 
 
@@ -60,10 +66,12 @@ int main(int argc, char **argv)
 	ros::Subscriber sub_rpwc_gripper_cmd = nh.subscribe("rpwc_EE_cmd", 1, &callback_rpwc_gripper_cmd);
   	//Publisher
     pub_hand_des_ = nh.advertise<trajectory_msgs::JointTrajectory>("qbhand/control/qbhand_synergy_trajectory_controller/command", 1);
-    pub_CommandHand_test_ = nh.advertise<std_msgs::Float64>("/hand_cmd", 1);
+    pub_for_recording_ = nh.advertise<rpwc_msgs::RobotEeStateStamped>("rpwc_recording_EE", 1);
+	
+    //pub_CommandHand_test_ = nh.advertise<std_msgs::Float64>("/hand_cmd", 1);
 
 	//Service Server
-  	ros::ServiceServer server_rpwc_gripper_cmd = nh.advertiseService("rpwc_EE_single_cmd", &callback_rpwc_gripper_single_cmd);
+  	//ros::ServiceServer server_rpwc_gripper_cmd = nh.advertiseService("rpwc_EE_single_cmd", &callback_rpwc_gripper_single_cmd);
 
 
 
