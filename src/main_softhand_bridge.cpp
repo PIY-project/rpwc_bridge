a6ac6904ccb2631387f7d3c8df25553e44d297a6
gialen <g.lentini88@gmail.com>
2024-05-15 15:22:09 +0200

    update

--- src/main_softhand_bridge.cpp
+++ src/main_softhand_bridge.cpp
@@ -1,7 +1,7 @@
 
 #include <ros/ros.h>
 #include <ros/rate.h>
-//#include <rpwc/rpwc_gripper_cmd.h>
+#include <rpwc_msgs/robotEeCmd.h>
 #include <rpwc_msgs/RobotEeStateStamped.h>
 #include <rpwc_msgs/robotEeState.h>
 #include <std_msgs/Float64.h>
@@ -11,6 +11,31 @@
 trajectory_msgs::JointTrajectory send_hand_;
 ros::Publisher pub_for_recording_, pub_hand_des_, pub_CommandHand_test_;
 rpwc_msgs::RobotEeStateStamped lastCmdMsg_;
+
+bool callback_rpwc_gripper_single_cmd(rpwc_msgs::robotEeCmd::Request  &req, rpwc_msgs::robotEeCmd::Response &res)
+{
+  	double position = req.state.position.data;
+	if(req.state.position.data < 0.0)position = 0.0;
+	else if (req.state.position.data > 1.0) position = 1.0;
+
+	double velocity = 1;
+	double force = 1;
+
+	send_hand_.points.clear();
+  	send_hand_.points.resize(1);
+  	send_hand_.points[0].positions.push_back(position);
+  	send_hand_.points[0].time_from_start = ros::Duration(0.3);
+
+	
+	lastCmdMsg_.position.data = position;
+	lastCmdMsg_.velocity.data = velocity;
+	lastCmdMsg_.force.data = force;
+	lastCmdMsg_.header.stamp = ros::Time::now();
+
+	pub_hand_des_.publish(send_hand_);
+
+	return true;
+}
 
 void callback_rpwc_gripper_cmd(const rpwc_msgs::RobotEeStateStamped::ConstPtr& msg)
 {
@@ -75,6 +100,7 @@ int main(int argc, char **argv)
     pub_hand_des_ = nh.advertise<trajectory_msgs::JointTrajectory>("qbhand/control/qbhand_synergy_trajectory_controller/command", 1);
     pub_for_recording_ = nh.advertise<rpwc_msgs::RobotEeStateStamped>("rpwc_recording_EE", 1);
 	//Service Server
+	ros::ServiceServer server_rpwc_gripper_cmd = nh.advertiseService("rpwc_EE_cmd", &callback_rpwc_gripper_single_cmd);
   	ros::ServiceServer server_robot_curr_pose = nh.advertiseService("rpwc_robot_curr_pose", &callback_robot_curr_pose);
 	
 	while(ros::ok())
