d335f727587a8d8e2721adcedb09ebba2697736f
gialen <g.lentini88@gmail.com>
2025-10-22 12:09:33 +0200

    tolte le action per il research di rpwc

--- src/main_softhand_bridge.cpp
+++ src/main_softhand_bridge.cpp
@@ -1,13 +1,11 @@
 #include <ros/ros.h>
 #include <ros/rate.h>
-#include <rpwc_msgs/robotEeCmdAction.h>
 #include <rpwc_msgs/robotEeCmd.h>
 #include <rpwc_msgs/RobotEeStateStamped.h>
 #include <rpwc_msgs/robotEeState.h>
 #include <std_msgs/Float64.h>
 #include <trajectory_msgs/JointTrajectory.h>
 #include <sensor_msgs/JointState.h>
-#include <actionlib/server/simple_action_server.h>
 #include <thread>
 #include <iostream>
 
@@ -15,40 +13,9 @@
 trajectory_msgs::JointTrajectory cmd_msg_;
 ros::Publisher pub_for_recording_, pub_gripper_des_;
 rpwc_msgs::RobotEeStateStamped lastCmdMsg_;
-typedef actionlib::SimpleActionServer<rpwc_msgs::robotEeCmdAction> robotEeCmdAS;
 double gripperFeedbackState_ = 0.0;
 
 bool inizialized_ = false;
-
-class eeCmdMove
-{
-	protected:
-		robotEeCmdAS as_;
-		rpwc_msgs::robotEeCmdGoalConstPtr ee_goal_;
-		rpwc_msgs::robotEeCmdResult ee_result_;
-		rpwc_msgs::robotEeCmdFeedback ee_feedback_;
-
-public:
-	eeCmdMove(ros::NodeHandle& nh_);
-	
-  	~eeCmdMove();
-
-  void goal_callback();
-  void preempt_callback();
-};
-
-eeCmdMove::eeCmdMove(ros::NodeHandle& nh_) : as_(nh_, "rpwc_EE_action_cmd", false)
-{
-	as_.registerGoalCallback(boost::bind(&eeCmdMove::goal_callback, this));
-	as_.registerPreemptCallback(boost::bind(&eeCmdMove::preempt_callback, this));
-	as_.start();
-  	ROS_INFO_STREAM("eeCmdMove action server avviato.");
-}
-
-eeCmdMove::~eeCmdMove()
-{
-    ROS_INFO_STREAM("eeCmdMove distrutto.");
-}
 
 bool callback_server_rpwc_gripper_cmd(rpwc_msgs::robotEeCmd::Request  &req, rpwc_msgs::robotEeCmd::Response &res)
 {
@@ -73,40 +40,6 @@ bool callback_server_rpwc_gripper_cmd(rpwc_msgs::robotEeCmd::Request  &req, rpwc
 	lastCmdMsg_.header.stamp = ros::Time::now();
 
 	return true;
-}
-
-void eeCmdMove::goal_callback()
-{
-    ee_goal_ = as_.acceptNewGoal();
-	ROS_INFO_STREAM("Received goal with force: " << ee_goal_->state.force.data
-                    << ", position: " << ee_goal_->state.position.data
-                    << ", velocity: " << ee_goal_->state.velocity.data);
-
-	rpwc_msgs::robotEeCmd::Request robotEeCmdSrv;
-	robotEeCmdSrv.state = ee_goal_->state;
-	rpwc_msgs::robotEeCmd::Response emptyResponse;
-	
-	if(!callback_server_rpwc_gripper_cmd(robotEeCmdSrv, emptyResponse))
-	{
-		ee_result_.success = false;
-		ee_result_.msg = "Error during command execution.";
-		as_.setAborted(ee_result_);
-		return;
-	}
-
-	ros::Duration(1.0).sleep(); //ensure the action is completed
-
-	ee_result_.success = true;
-	ee_result_.msg = "Execution completed successfully.";
-	as_.setSucceeded(ee_result_);
-
-	return;
-}
-
-void eeCmdMove::preempt_callback()
-{
-    ROS_WARN("Goal preempted");
-    as_.setPreempted();
 }
 
 
@@ -205,7 +138,6 @@ int main(int argc, char **argv)
   	ros::ServiceServer server_robot_curr_pose = nh.advertiseService("rpwc_robot_curr_pose", &callback_robot_curr_pose);
 	ros::ServiceServer server_robot_curr_state = nh.advertiseService("rpwc_robot_curr_state", &callback_robot_curr_state);
 
-	eeCmdMove ee_cmd_move(nh);
 
 	// fare un thread con pub for recording a 10 hz
 	std::thread pubForRecordingThread(&pub_for_recording_callback);
